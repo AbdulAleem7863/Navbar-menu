64cd4cdddb296a65b6f63d83087a4a26bc57a283
Abdul Aleem <abdulaleemnaqshbandi1234@gmail.com>
2024-11-03 22:48:15 +0500

    NAVBAR MENU COMPLETED

---
+++ src/components/Navbar/MyLinks.jsx
@@ -0,0 +1,170 @@
+export const links = [
+    {
+        name: "Men",
+        submenu: true,
+        sublinks: [
+            {
+                Head: "TopWear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Bottom Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Inner Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "sleep Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Foot Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+        ]
+    },
+    {
+        name: "Women",
+        submenu: true,
+        sublinks: [
+            {
+                Head: "TopWear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Bottom Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Inner Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "sleep Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Foot Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+        ]
+    },
+    {
+        name: "Kid's",
+        submenu: true,
+        sublinks: [
+            {
+                Head: "TopWear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Bottom Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Inner Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "sleep Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+            {
+                Head: "Foot Wear",
+                sublink: [
+                    { name: 'T-shirt', link: "/" },
+                    { name: 'Casual Shirts', link: "/" },
+                    { name: 'Formal Shirts', link: "/" },
+                    { name: 'T-shirt', link: "/" },
+                ]
+
+            },
+        ]
+    },
+]
--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -1,186 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { links } from './MyLinks'
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+
 
 const NavLinks = () => {
-
-    const links = [
-        {
-            name: "Men",
-            submenu: true,
-            sublinks: [
-                {
-                    Head: "TopWear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Bottom Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Inner Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "sleep Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Foot Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-            ]
-        },
-        {
-            name: "Women",
-            submenu: true,
-            sublinks: [
-                {
-                    Head: "TopWear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Bottom Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Inner Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "sleep Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Foot Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-            ]
-        },
-        {
-            name: "Kid's",
-            submenu: true,
-            sublinks: [
-                {
-                    Head: "TopWear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Bottom Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Inner Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "sleep Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-                {
-                    Head: "Foot Wear",
-                    sublink: [
-                        { name: 'T-shirt', link: "/" },
-                        { name: 'Casual Shirts', link: "/" },
-                        { name: 'Formal Shirts', link: "/" },
-                        { name: 'T-shirt', link: "/" },
-                    ]
-
-                },
-            ]
-        },
-    ]
-
+    const [heading, setHeading] = useState("")
+    const [subHeading, setSubHeading] = useState("")
     return (
         <>
             {links.map(link => (
                 <div>
                     <div className='px-3 text-left md:cursor-pointer group'>
-                        <h1 className='py-7'>{link.name}</h1>
+                        <h1 className='py-7  flex justify-between items-center md:pr-0 pr-5 group ' onClick={() => {
+                            heading !== link.name ? setHeading(link.name) : setHeading("");
+                            setSubHeading("")
+                        }}>{link.name}
+                            <span className='text-xl md:hidden inline'>
+                                {heading === link.name ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+
+                            </span>
+                            <span className='text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2'>
+                                 <MdKeyboardArrowDown />
+
+                            </span>
+                        </h1>
+
+
                         {link.submenu && <div>
-                            <div className='absolute top-22 hidden group-hover:block hover:block '>
+                            <div className='absolute top-22 hidden group-hover:md:block hover:md:block '>
                                 <div className='py-3'>
                                     <div className='w-4 h-4 left-3 absolute mt-1 bg-white rotate-45'></div>
                                 </div>
@@ -203,7 +50,36 @@ const NavLinks = () => {
                             </div>
                         </div>}
                     </div>
-                </div>
+
+                    {/* MOBILE MENU  */}
+
+                    <div className={`${heading === link.name ? "md:hidden" : "hidden"}`}>
+                        {link.sublinks.map((slinks) => {
+                            return (
+                                <div>
+                                    <div>
+                                        <h1 className='py-4 pl-7 font-semibold flex justify-between items-center md:pr-0 pr-5' onClick={() =>
+                                            subHeading !== slinks.Head ? setSubHeading(slinks.Head) : setSubHeading("")
+                                        }>{slinks.Head}
+                                            <span className='text-xl md:mt-1 md:ml-2 inline'>
+                                                {subHeading === slinks.Head ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+
+                                            </span>
+                                        </h1>
+                                        <div className={`${subHeading === slinks.Head ? "md:hidden" : "hidden"}`}>
+                                            {slinks?.sublink.map((slink) => (
+                                                <li className='py-3 pl-14'>
+                                                    <a href={slink.link} className='hover:text-blue-500'>{slink.name}</a>
+                                                </li>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div >
+
+                </div >
             ))}
         </>
     )
